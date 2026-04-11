76781fa0235cce8712f27744e9c6055e1ebacfd4
connect4deepak <connect4deepak@gmail.com>
2026-04-11 23:49:07 +0100

    test

--- transforms.py
+++ transforms.py
@@ -7,3 +7,12 @@ from sklearn.preprocessing import MinMaxScaler
 
 logger = logging.getLogger(__name__)
 SCALER_PATH = "scaler.pkl"
+
+# Numeric Scaling 
+SCALE_COLS = ["magnitude", "depth_km", "latitude", "longitude"]
+SCALE_OUTPUT = {
+    "magnitude":  "magnitude_scaled",
+    "depth_km":   "depth_scaled",
+    "latitude":   "latitude_scaled",
+    "longitude":  "longitude_scaled",
+}
