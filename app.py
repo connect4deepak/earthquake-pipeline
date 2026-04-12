7f77b0cc18ea8194f01b0bb0b0d042a5cd0e3092
connect4deepak <connect4deepak@gmail.com>
2026-04-12 12:30:34 +0100

    setup pipeline directory

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ from flask import Flask, render_template, jsonify, request
 import psycopg2
 import psycopg2.extras
 
+PIPELINE_DIR = "/home/ubuntu/earthquake-pipelibe"
+sys.path.insert(0, PIPELINE_DIR)
+from config import DB_CONFIG, PROCESSED_TABLE, RAW_TABLE
+app = Flask(__name__)
