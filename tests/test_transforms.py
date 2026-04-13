323a1be41e73a7c5334dd6b98c64bf9eb0b87190
connect4deepak <connect4deepak@gmail.com>
2026-04-13 19:42:44 +0100

    test

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -6,3 +6,12 @@ import numpy as np
 import pandas as pd
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import transforms as _transforms_mod
+
+SCALER_PATH = os.path.join(os.path.dirname(__file__), 'test_scaler.pkl')
+_transforms_mod.SCALER_PATH = SCALER_PATH 
+
+def _remove_scaler():
+    if os.path.exists(SCALER_PATH):
+        os.remove(SCALER_PATH)
+
+from transforms import scale_numeric, encode_categorical, select_final_columns, run_transforms, FINAL_COLUMNS
